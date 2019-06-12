b0b27364ea8bfe7b810e15e62598fad93e904fd8
Pavlo Kvas <kvaspasha@gmail.com>
2019-06-12 09:46:35 +0300

    add timer closes #6

--- Assets/Scripts/AffirmationPlayer.cs
+++ Assets/Scripts/AffirmationPlayer.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using Random = System.Random;
 
 namespace AffirmationPlayer
 {
     public class AffirmationPlayer : MonoBehaviour
     {
+        [SerializeField] private TMP_Text remainingTimeText;
+        [SerializeField] private TMP_InputField durationInputField;
+
         [SerializeField] private TextMeshProUGUI affirmationText;
-        
+
         [SerializeField] private Affirmations affirmations;
-        
+
         private Stack<int> affirmationIndexStack;
+
+        private double timeElapsed;
+
+
+        [SerializeField]
+        private UnityEvent sessionComplete;
 
         public List<string> Affirmations
         {
@@ -23,12 +37,48 @@ namespace AffirmationPlayer
 
         public void Play()
         {
+            if (!int.TryParse(durationInputField.text, out int durationMinutes))
+            {
+                Debug.LogError("can't parse duration text");
+                return;
+            }
+
+            StartTimer(durationMinutes*60);
             affirmations.LoadLines();
 
             GenerateRandomIndexSequence();
         
             ShowNext();
         }
+
+        private void StartTimer(int durationSeconds)
+        {
+            
+            StartCoroutine(SessionTimer(durationSeconds));
+        }
+
+        private IEnumerator SessionTimer(int durationSeconds)
+        {
+            int remainingTime = durationSeconds;
+            SetRemainingTimeText(remainingTime);
+            do
+            {
+                yield return new WaitForSeconds(1);
+
+                remainingTime -= 1;
+                SetRemainingTimeText(remainingTime);
+            } while (remainingTime > 0);
+            
+            yield return new WaitForSeconds(.5f);
+            
+            sessionComplete.Invoke();
+        }
+
+        private void SetRemainingTimeText(int remainingTime)
+        {
+            remainingTimeText.text = $"{remainingTime / 60:00}:{remainingTime % 60:00}";
+        }
+
 
         private void GenerateRandomIndexSequence()
         {
@@ -48,6 +98,5 @@ namespace AffirmationPlayer
             var nextIndex = affirmationIndexStack.Pop();
             affirmationText.text = Affirmations[nextIndex];
         }
-      
     }
 }
